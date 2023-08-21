285107baf72f062591583bcc3ad5fa651af240fc
joshualan <joshualan23@gmail.com>
2023-08-21 17:20:06 -0400

    add icons to sidebar

--- frontend/src/features/side-nav-bar/side-nav-bar.tsx
+++ frontend/src/features/side-nav-bar/side-nav-bar.tsx
@@ -1,12 +1,25 @@
 import { AlanCard } from "components/alan-card";
 import {
-  Box,
+  Button,
   Stack,
   Typography,
   TypographyProps,
   styled,
 } from "lib/component-library";
 import { Image } from "mui-image";
+import {
+  Explore,
+  Bolt,
+  Description,
+  Mic,
+  WatchLater,
+  PlayCircle,
+  Favorite,
+  AddBox,
+  SystemUpdateAlt,
+  Settings,
+  Logout,
+} from "lib/icons";
 
 const StyledSubHeading = styled(Typography)<TypographyProps>(({ theme }) => ({
   color: theme.palette.grey[500],
@@ -30,8 +43,23 @@ export const SideNavBar = () => {
         </div>
         <div>
           <StyledSubHeading align="left">MENU</StyledSubHeading>
+          <Button startIcon={<Explore />}>Discover</Button>
+          <Button startIcon={<Bolt />}>Trending</Button>
+          <Button startIcon={<Description />}>Genres</Button>
+          <Button startIcon={<Mic />}>Speaker</Button>
+
+          <StyledSubHeading align="left">Library</StyledSubHeading>
+          <Button startIcon={<WatchLater />}>Resent</Button>
+          <Button startIcon={<PlayCircle />}>Playlist</Button>
+          <Button startIcon={<Favorite />}>Favorites</Button>
+
+          <StyledSubHeading align="left">Customize</StyledSubHeading>
+          <Button startIcon={<AddBox />}>New Playlist</Button>
+          <Button startIcon={<SystemUpdateAlt />}>Download</Button>
+          <Button startIcon={<Settings />}>Settings</Button>
+
+          <Button startIcon={<Logout />}>Log Out</Button>
         </div>
-        <div>Logout</div>
       </Stack>
     </AlanCard>
   );
